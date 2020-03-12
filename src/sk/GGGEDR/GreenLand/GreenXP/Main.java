aa032b148e2a89c4ce1002d3b03958b8c5141ec4
[IS] <GGGEDR@spigotmc.org>
2020-03-12 15:45:43 +0100

    TEST

--- src/sk/GGGEDR/GreenLand/GreenXP/Main.java
+++ src/sk/GGGEDR/GreenLand/GreenXP/Main.java
@@ -1,4 +1,22 @@
 package sk.GGGEDR.GreenLand.GreenXP;
 
-public class Main {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    public static String prefix = "§7[§aGreenXP§7] ";
+
+    public void send(String msg){
+        this.getLogger().info(prefix + msg);
+    }
+
+    @Override
+    public void onEnable(){
+        send("Plugin bol úspešne spustený");
+    }
+
+    @Override
+    public void onDisable(){
+        send("Plugin bol úspešne vypnutí");
+    }
 }
