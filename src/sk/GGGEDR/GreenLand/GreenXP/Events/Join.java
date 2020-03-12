378eb53c1bb766caa04af19e1f16b17578ba0aaf
[IS] <GGGEDR@spigotmc.org>
2020-03-12 16:31:42 +0100

    Added more methods to Join.onJoin

--- src/sk/GGGEDR/GreenLand/GreenXP/Events/Join.java
+++ src/sk/GGGEDR/GreenLand/GreenXP/Events/Join.java
@@ -1,6 +1,7 @@
 package sk.GGGEDR.GreenLand.GreenXP.Events;
 
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerJoinEvent;
 import sk.GGGEDR.GreenLand.GreenXP.Main;
@@ -14,8 +15,19 @@ public class Join {
             e.setJoinMessage(Main.prefix +"§7Práve sa pripojil §a"+ e.getPlayer().getName() +" §7(§a"+ Bukkit.getServer().getOnlinePlayers().size() +"§7/§a"+ Bukkit.getMaxPlayers() +"§7)");
             int vypocet = Bukkit.getMaxPlayers() / 4;
             int prepocet = vypocet * 3;
+            e.getPlayer().setGameMode(GameMode.SURVIVAL);
             if(prepocet == Bukkit.getOnlinePlayers().size()){
                 ArenaStatus.setStatus(ArenaStatus.WAIT);
+            }
+        } else if(ArenaStatus.getStatus() == ArenaStatus.WAIT){
+            e.setJoinMessage(Main.prefix +"§7Práve sa pripojil §a"+ e.getPlayer().getName() +" §7(§a"+ Bukkit.getServer().getOnlinePlayers().size() +"§7/§a"+ Bukkit.getMaxPlayers() +"§7)");
+            e.getPlayer().setGameMode(GameMode.SURVIVAL);
+        } else if(ArenaStatus.getStatus() == ArenaStatus.INGAME) {
+            if (e.getPlayer().hasPermission("gl.vip") || e.getPlayer().hasPermission("gl.friend")) {
+                e.setJoinMessage(" ");
+                e.getPlayer().setGameMode(GameMode.SPECTATOR);
+            } else {
+                e.getPlayer().kickPlayer(Main.prefix + "Prepáč nemôžeš spectatovať pokiaľ nemáš §a§lVIP");
             }
         }
     }
